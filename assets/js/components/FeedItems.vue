2f5439a1b3e4558eda40f2ecff16552522663693
Igor Makarsky <makarrski@gmail.com>
2020-02-02 21:58:53 +0300

    Add FeedItems component

---
+++ assets/js/components/FeedItems.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <v-row v-for="feedItem in feedItems" v-bind:key="feedItem.link">
+            <v-col>
+                <v-card class="">
+                    <v-toolbar flat>
+                        <v-toolbar-title>{{feedItem.title}}</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text v-html="feedItem.description">
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer/>
+                        <a :href="feedItem.link" target="_blank">Read on</a>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FeedItems',
+        props: {
+            feedItems: [],
+        },
+    }
+</script>
+
+<style scoped>
+</style>
--- assets/js/views/Feed.vue
+++ assets/js/views/Feed.vue
@@ -26,30 +26,20 @@
                     v-show="!isLoading"
                     :frequent-words="frequentWords"
             />
-            <v-row v-for="feedItem in feedItems" v-bind:key="feedItem.link">
-                <v-col>
-                    <v-card class="">
-                        <v-toolbar flat>
-                            <v-toolbar-title>{{feedItem.title}}</v-toolbar-title>
-                        </v-toolbar>
-                        <v-card-text v-html="feedItem.description">
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer/>
-                            <a :href="feedItem.link" target="_blank">Read on</a>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-            </v-row>
+            <feed-items
+                    :feed-items="feedItems"
+            />
         </v-col>
     </v-row>
 </template>
 
 <script>
     import FrequentWords from '../components/FrequentWords';
+    import FeedItems from '../components/FeedItems';
+
     export default {
         name: 'Feed',
-        components: {FrequentWords},
+        components: {FrequentWords, FeedItems},
         data() {
             return {
                 feedItems: [],
