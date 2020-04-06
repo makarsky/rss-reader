bdb4f59225c2e848a0af2216e08957683caea04f
Igor Makarsky <makarrski@gmail.com>
2020-04-06 09:24:21 +0300

    Set up test environment

---
+++ tests/Service/RssWordCounterServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\RssWordCounterService;
+use PHPUnit\Framework\TestCase;
+
+class RssWordCounterServiceTest extends TestCase
+{
+    public function testAdd()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
